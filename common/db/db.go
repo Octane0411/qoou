94879dc8148426a3b38b83eba1359d0bc88551cf
Octane0411 <wdznb1@gmail.com>
2022-03-25 15:31:34 +0800

    测试了github的API

---
+++ common/db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func NewDbEngine() *gorm.DB {
+	dsn := "root:123456@tcp(127.0.0.1:3305)/qoou?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
---
+++ util/encode.go
@@ -0,0 +1,8 @@
+package util
+
+import "encoding/base64"
+
+// encode string to base64
+func EncodeBase64(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
