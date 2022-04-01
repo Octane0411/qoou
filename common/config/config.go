2c0d5ff303e01a698f59d977a21f366d60b678ff
Octane0411 <wdznb1@gmail.com>
2022-04-01 23:10:31 +0800

    引入logrus,viper

---
+++ common/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"github.com/Octane0411/qoou/common/logger"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+)
+
+var ServerSetting *ServerSettingS
+
+func setupSetting() {
+	setting, err := NewSetting()
+	if err != nil {
+		logger.Logger.Panic(err)
+	}
+	err = setting.ReadSection("Server", &ServerSetting)
+	if err != nil {
+		logger.Logger.Panic(err)
+	}
+}
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	configDir := GetConfigPath()
+	vp.AddConfigPath(configDir)
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Setting{
+		vp: vp,
+	}, nil
+}
+
+func GetConfigPath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		logger.Logger.Panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	rootPath, err := filepath.EvalSymlinks(exPath)
+	configPath := filepath.Join(rootPath, "config")
+	if err != nil {
+		logger.Logger.Panic(err)
+	}
+	return configPath
+}
---
+++ common/config/section.go
@@ -0,0 +1,14 @@
+package config
+
+type ServerSettingS struct {
+	Port string
+}
+
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
