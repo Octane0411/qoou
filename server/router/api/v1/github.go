7ddb9cc72b1a947986f2678b9ae9825907a01305
Octane0411 <wdznb1@gmail.com>
2022-04-21 22:36:05 +0800

    增加log功能，优化反向代理代码

---
+++ server/router/api/v1/github.go
@@ -0,0 +1,176 @@
+package v1
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/Octane0411/qoou/common/db"
+	"github.com/Octane0411/qoou/common/global"
+	"github.com/Octane0411/qoou/common/logger"
+	"github.com/Octane0411/qoou/server/dao"
+	"github.com/Octane0411/qoou/server/model"
+	"github.com/Octane0411/qoou/server/service"
+	"github.com/Octane0411/qoou/util"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+)
+
+type TokenRequest struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
+type GitHubCreateRepoRequest struct {
+	Name              string `json:"name"`
+	AutoInit          bool   `json:"auto_init"`
+	Private           bool   `json:"private"`
+	GitignoreTemplate string `json:"gitignore_template"`
+}
+
+type CreateRepoRequest struct {
+	Username string `json:"username"`
+	RepoName string `json:"repoName"`
+}
+
+type CreateRepoWithTemplateRequest struct {
+	Username string `json:"username"`
+	RepoName string `json:"repoName"`
+	Template string `json:"template"`
+}
+
+type GitHubErrorResponse struct {
+	Message string `json:"message"`
+}
+
+// https://docs.github.com/cn/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+// 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
+func LoginGithubWithToken(c *gin.Context) {
+	tr := &TokenRequest{}
+	c.BindJSON(tr)
+	client := &http.Client{}
+	requestBody := fmt.Sprintf(`{
+	"name": "%s"
+	}`, "gd-2")
+	var jsonStr = []byte(requestBody)
+	req, err := http.NewRequest("POST", "https://api.github.com/repos/octane0411/go-demo/generate", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	//req.Header.Set("Authorization", "Basic "+base64Head)
+
+	//req, err := http.NewRequest("GET", "https://api.github.com/users/"+tr.Username, nil)
+	req.Header.Set("Authorization", "token "+tr.Token)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func RegisterWithGitHubToken(c *gin.Context) {
+	tr := &TokenRequest{}
+	err := c.BindJSON(tr)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"msg": "请求格式错误",
+		})
+		return
+	}
+	//验证是否注册过
+	user := &model.User{
+		Username: tr.Username,
+		Token:    tr.Token,
+	}
+	ok := dao.FindUser(tr.Username)
+	if ok {
+		db.DB.Updates(user)
+		c.JSON(200, gin.H{"msg": "token已更新"})
+		return
+	}
+	//新增用户
+	db.DB.Create(&user)
+	c.JSON(200, gin.H{"msg": "注册成功"})
+}
+
+func CreateRepoWithTemplate(c *gin.Context) {
+	req := &CreateRepoWithTemplateRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		logger.Logger.Error(err)
+	}
+	template, ok := global.TemplateMap[req.Template]
+	if !ok {
+		c.JSON(200, gin.H{"msg": "模板不存在"})
+		return
+	}
+
+	// TODO:封装到service中，并且另起一个协程
+	// 写入数据库
+	t := util.GetZeroTime()
+	err = dao.CreateProject(&model.Project{
+		Username:   req.Username,
+		RepoName:   req.RepoName,
+		Template:   req.Template,
+		LastCommit: t,
+		LastRun:    t,
+	})
+	if err != nil {
+		logger.Logger.Error(err)
+		c.JSON(500, gin.H{"msg": "写入数据库失败"})
+		return
+	}
+
+	// 创建仓库
+	gReq := GetGitHubCreateRepoWithTemplateReq(service.GetTokenByUsername(req.Username), req.RepoName, template[0], template[1])
+	resp, err := Cli.Do(gReq)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Logger.Error(err)
+	}
+	gResp := &GitHubErrorResponse{}
+	json.Unmarshal(body, gResp)
+	if gResp.Message != "" {
+		logger.Logger.Debugln(gResp.Message)
+		c.JSON(500, gin.H{"msg": gResp.Message})
+		return
+	}
+
+	c.JSON(200, gin.H{"msg": "创建成功"})
+}
+
+func GetGitHubAuthReq(username, token string) *http.Request {
+	head := username + token
+	base64Head := util.EncodeBase64(head)
+	fmt.Println(base64Head)
+	req := &http.Request{}
+	req.Header.Set("Authorization", "Basic "+base64Head)
+	return req
+}
+
+func GetGitHubAuthReqWithToken(username, token string) *http.Request {
+	req, err := http.NewRequest("GET", "https://api.github.com/users/"+username, nil)
+	if err != nil {
+		logger.Logger.Error(err)
+	}
+	req.Header.Set("Authorization", "token "+token)
+	return req
+}
+
+func GetGitHubCreateRepoWithTemplateReq(token, name, tempOwner, tempName string) *http.Request {
+	body := bytes.NewBuffer([]byte(fmt.Sprintf(`{
+	"name": "%s"
+	}`, name)))
+	req, err := http.NewRequest("POST", "https://api.github.com/repos/"+tempOwner+"/"+tempName+"/generate", body)
+	if err != nil {
+		logger.Logger.Error(err)
+	}
+	req.Header.Set("Authorization", "token "+token)
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	return req
+}
---
+++ server/router/router.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"github.com/Octane0411/qoou/server/middleware"
+	v1 "github.com/Octane0411/qoou/server/router/api/v1"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.CORSMiddleware())
+
+	apiv1 := r.Group("/api/v1")
+
+	{
+		apiv1.POST("/register", v1.RegisterWithGitHubToken)
+
+		apiv1.POST("/repo", v1.CreateRepoWithTemplate)
+
+		apiv1.POST("/project/deploy", v1.Deploy)
+		apiv1.GET("/project/", v1.GetProjectsByUsername)
+		apiv1.GET("/project/start", v1.StartContainer)
+		apiv1.GET("/project/stop", v1.StopContainer)
+		apiv1.GET("/project/preview/:username/:repoName/*all", v1.Forward)
+
+		apiv1.GET("/project/log", v1.GetLog)
+
+	}
+
+	return r
+}
